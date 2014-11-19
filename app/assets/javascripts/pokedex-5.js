0a5721c3d74b93b8fe487d2bc4bde685e2051647
Reza Farajian <reza.farajian@gmail.com>
2014-11-18 17:48:10 -0800

    create routes and views for pokemon and toys details

--- app/assets/javascripts/pokedex-5.js
+++ app/assets/javascripts/pokedex-5.js
@@ -2,47 +2,81 @@ Pokedex.Views = {}
 
 Pokedex.Views.PokemonIndex = Backbone.View.extend({
   events: {
+    "click li": "selectPokemonFromList"
   },
 
   initialize: function () {
+    this.collection = new Pokedex.Collections.Pokemon();
   },
 
   addPokemonToList: function (pokemon) {
+    this.$el.append(JST["pokemonListItem"]({ pokemon: pokemon }));
   },
 
-  refreshPokemon: function (options) {
+  refreshPokemon: function (callback) {
+    this.collection.fetch({
+      success: (function() {
+        this.render();
+        if (callback) {          
+          callback();
+        }
+      }).bind(this)
+    });
   },
 
   render: function () {
+    this.$el.empty();
+    this.collection.each(this.addPokemonToList.bind(this));
   },
 
   selectPokemonFromList: function (event) {
+    var id = $(event.target).attr("data-id");
+    Backbone.history.navigate("pokemon/" + id, { trigger: true})
   }
 });
 
 Pokedex.Views.PokemonDetail = Backbone.View.extend({
   events: {
+    "click li": "selectToyFromList"
   },
 
-  refreshPokemon: function (options) {
+  refreshPokemon: function (callback) {
+    this.model.fetch({
+      success: (function () {
+        this.render();
+        if(callback) {
+          callback();
+        }
+      }).bind(this)
+    });
   },
 
   render: function () {
+    this.$el.append(JST["pokemonDetail"]({ pokemon: this.model }));
+    var $ul = this.$el.find("ul.toys");
+    this.model.toys().each(function (toy) {
+      $ul.append(JST["toyListItem"]({toy: toy}))
+    })
   },
 
   selectToyFromList: function (event) {
+    var toyId = $(event.target).attr("data-id");
+    var pokemonId = $(event.target).attr("data-pokemon-id");
+    Backbone.history.navigate("pokemon/" + pokemonId + "/toys/" + toyId, { trigger: true})
   }
 });
 
 Pokedex.Views.ToyDetail = Backbone.View.extend({
   render: function () {
+    // debugger
+    this.$el.html(JST["toyDetail"]({ toy: this.model, pokes: new Pokedex.Collections.Pokemon()}))
   }
 });
 
-/*
-$(function () {
-  var pokemonIndex = new Pokedex.Views.PokemonIndex();
-  pokemonIndex.refreshPokemon();
-  $("#pokedex .pokemon-list").html(pokemonIndex.$el);
-});
-*/
+
+// $(function () {
+//   var pokemonIndex = new Pokedex.Views.PokemonIndex();
+//   pokemonIndex.refreshPokemon();
+//   $("#pokedex .pokemon-list").html(pokemonIndex.$el);
+// });
+
--- app/assets/javascripts/pokedex-6.js
+++ app/assets/javascripts/pokedex-6.js
@@ -1,20 +1,47 @@
 Pokedex.Router = Backbone.Router.extend({
   routes: {
+    "": "pokemonIndex",
+    "pokemon/:id": "pokemonDetail",
+    "pokemon/:pokemonId/toys/:toyId": "toyDetail"
   },
 
   pokemonDetail: function (id, callback) {
+    if (this._pokemonIndex) {
+      var pokemon = this._pokemonIndex.collection.get(id);
+      this._pokemonDetail = new Pokedex.Views.PokemonDetail({ model: pokemon });
+      $("#pokedex .pokemon-detail").html(this._pokemonDetail.$el);
+      this._pokemonDetail.refreshPokemon(callback);
+    } else {
+      this.pokemonIndex(function () {
+        this.pokemonDetail(id, callback);
+      }.bind(this))
+    }
   },
 
-  pokemonIndex: function (callback) {
+  pokemonIndex: function (callback) { 
+    this._pokemonIndex = new Pokedex.Views.PokemonIndex();
+    this._pokemonIndex.refreshPokemon(callback);
+    $("#pokedex .pokemon-list").html(this._pokemonIndex.$el);
   },
 
   toyDetail: function (pokemonId, toyId) {
+    if (this._pokemonDetail) {
+      var toy = this._pokemonDetail.model.toys().get(toyId);
+      var toyDetail = new Pokedex.Views.ToyDetail({ model: toy });
+      $("#pokedex .toy-detail").html(toyDetail.$el);
+      toyDetail.render();
+    } else {
+      this.pokemonDetail(pokemonId, 
+        this.toyDetail.bind(this, pokemonId, toyId)
+      );
+    }
+    
   }
 });
 
-/*
+
 $(function () {
   new Pokedex.Router();
   Backbone.history.start();
 });
-*/
+
